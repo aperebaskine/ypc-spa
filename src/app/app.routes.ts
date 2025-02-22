eb2ab3a2e7b597012e2d0130dd47685b758df4bb
Alexandre Perebaskine <pereb27@gmail.com>
2025-02-22 10:43:14 +0100

    Add routes for main page and product search page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './components/main/main-page/main-page.component';
+import { ProductSearchComponent } from './components/main/product-search/product-search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MainPageComponent },
+    { path: 'product-search', component: ProductSearchComponent },
+];
