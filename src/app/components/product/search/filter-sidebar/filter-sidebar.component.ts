b283a6bb292507d42fefc7db6121cd99e6a705a8
Alexandre Perebaskine <pereb27@gmail.com>
2025-02-22 15:54:07 +0100

    Retrieve categories for display in select

--- src/app/components/product/search/filter-sidebar/filter-sidebar.component.ts
+++ src/app/components/product/search/filter-sidebar/filter-sidebar.component.ts
@@ -1,13 +1,16 @@
 import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { CategoryService } from '../../../../services/category.service';
 import { CategoryDTO, DefaultService } from '../../../../generated';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-filter-sidebar',
   imports: [
+    CommonModule,
     ReactiveFormsModule,
     MatInputModule,
     MatSelectModule
@@ -16,6 +19,13 @@ import { CategoryDTO, DefaultService } from '../../../../generated';
   styleUrl: './filter-sidebar.component.css'
 })
 export class FilterSidebarComponent {
+
+  categories: CategoryDTO[] = [];
+
+  constructor(private categoryService: CategoryService) {
+    this.categoryService.findAll("en-GB")
+      .subscribe((categories) => this.categories = categories);
+  }
 
   searchForm = new FormGroup({
     name: new FormControl(''),
