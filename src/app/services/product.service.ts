41b6e3b1696bc83e1f979ed6c7f1bd8369084ea4
Alexandre Perebaskine <aperebaskine@dip.local>
2025-02-24 20:06:05 +0100

    Implement product service facade

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { DefaultService, LightAttribute } from '../generated';
+import { FormGroup } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  constructor(private defaultService: DefaultService) {}
+
+  findById(id: number) {
+    return this.defaultService.findProductById('en-GB', id);
+  }
+
+  findBy(
+    pos: number,
+    pageSize: number,
+    filters: {
+      name?: string;
+      launchDateFrom?: string;
+      launchDateTo?: string;
+      hasStock?: boolean;
+      priceMin?: number;
+      priceMax?: number;
+      categoryId?: number;
+      attributes?: LightAttribute[];
+    }
+  ) {
+    return this.defaultService.findProductBy(
+      'en-GB',
+      pos,
+      pageSize,
+      filters.name,
+      filters.launchDateFrom,
+      filters.launchDateTo,
+      filters.hasStock === undefined ? undefined : Number(filters.hasStock),
+      undefined,
+      filters.priceMin,
+      filters.priceMax,
+      filters.categoryId,
+      filters.attributes
+    );
+  }
+}
