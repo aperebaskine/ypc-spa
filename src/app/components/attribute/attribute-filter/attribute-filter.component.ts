382bc16f0e92a130a75b1e9d5242d979174f5e8d
Alexandre Perebaskine <aperebaskine@dip.local>
2025-02-24 22:28:04 +0100

    Store attribute as property

--- src/app/components/attribute/attribute-filter/attribute-filter.component.ts
+++ src/app/components/attribute/attribute-filter/attribute-filter.component.ts
@@ -1,48 +1,15 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Attribute } from '../../../generated';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-attribute-filter',
-  imports: [],
+  imports: [MatDivider],
   templateUrl: './attribute-filter.component.html',
   styleUrl: './attribute-filter.component.css'
 })
 export class AttributeFilterComponent {
 
-  private static types = new Map<String, any>();
-
-  private attribute?: Attribute;
-
-
-  private constructor() { }
-
-  public static of(attribute: Attribute) {
-    let type: any = Promise.resolve(AttributeFilterComponent.resolveTemplate(attribute));
-    return new type.default(attribute);
-  }
-
-  private static async resolveTemplate(attribute: Attribute): Promise<any> {
-
-    let type: any = AttributeFilterComponent.types.get(attribute.dataType!);
-
-    if (type) {
-      return type;
-    }
-
-    try {
-      type = await import(`./data-type/${attribute.dataType}/${attribute.dataType}.component.js`);
-    } catch (e) {
-      try {
-        type = await import(`./handling-mode/${attribute.handlingMode}/${attribute.handlingMode}.component.js`);
-      } catch (e) {
-        throw new Error(`No component defined for attribute of data type 
-          ${attribute.dataType} or handling mode ${attribute.handlingMode}`);
-      }
-    }
-
-    AttributeFilterComponent.types.set(attribute.dataType!, type);
-
-    return type;
-  }
+  @Input() attribute?: Attribute;
 
 }
