73857a083d2b418bfeb47183a49a274a52f37d92
Alexandre Perebaskine <pereb27@gmail.com>
2025-02-25 14:20:31 +0100

    Restructure component directory

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { MainPageComponent } from './components/layout/main/main-page/main-page.component';
-import { ProductSearchComponent } from './components/layout/main/product-search/product-search.component';
+import { MainPageComponent } from './components/pages/main-page/main-page.component';
+import { ProductSearchComponent } from './components/pages/product-search/product-search.component';
 
 export const routes: Routes = [
     { path: '', component: MainPageComponent },
