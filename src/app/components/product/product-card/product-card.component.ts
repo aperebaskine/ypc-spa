746cf78e90f5a3208043826859f2d935720efa3a
Alexandre Perebaskine <pereb27@gmail.com>
2025-02-28 12:38:12 +0100

    Initial implementation of product card

--- src/app/components/product/product-card/product-card.component.ts
+++ src/app/components/product/product-card/product-card.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../../../generated';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
