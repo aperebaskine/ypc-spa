b68808c196f971d9d65b2adf03df407f43b12978
Alexandre Perebaskine <aperebaskine@dip.local>
2025-02-28 16:07:51 +0100

    Link view button to product details view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './components/pages/main-page/main-page.component';
 import { ProductSearchComponent } from './components/pages/product-search/product-search.component';
+import { ProductDetailsComponent } from './components/pages/product-details/product-details.component';
 
 export const routes: Routes = [
     { path: '', component: MainPageComponent },
     { path: 'product-search', component: ProductSearchComponent },
+    { path: 'product/:id', component: ProductDetailsComponent }
 ];
--- src/app/components/product/product-card/product-card.component.ts
+++ src/app/components/product/product-card/product-card.component.ts
@@ -2,15 +2,14 @@ import { Component, Input } from '@angular/core';
 import { Product } from '../../../generated';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, RouterModule],
   templateUrl: './product-card.component.html',
-  styleUrl: './product-card.component.scss'
+  styleUrl: './product-card.component.scss',
 })
 export class ProductCardComponent {
-
   @Input() product?: Product;
-
 }
