75c0c8dd3e2db39d040c5ef5df0543b87b8b997d
Alexandre Perebaskine <aperebaskine@dip.local>
2025-03-07 19:18:27 +0100

    WIP 07/03/2025

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,27 +1,39 @@
 import { Injectable } from '@angular/core';
-import { DefaultService } from '../generated';
+import { Customer, DefaultService } from '../generated';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationService {
+  private readonly userSubject!: BehaviorSubject<Customer | null>;
+  public readonly user!: Observable<Customer | null>;
 
-  private readonly loginStatusSubject!: BehaviorSubject<boolean>;
-  public readonly loginStatus!: Observable<boolean>;
-
-  constructor(private defaultService: DefaultService) { 
-    this.loginStatusSubject = new BehaviorSubject<boolean>(false);
-    this.loginStatus = this.loginStatusSubject.asObservable();
+  constructor(
+    private defaultService: DefaultService,
+    private localStorageService: LocalStorageService
+  ) {
+    this.userSubject = new BehaviorSubject<Customer | null>(null);
+    this.user = this.userSubject.asObservable();
   }
 
   login(email: string, password: string) {
-    this.defaultService.loginCustomer(email, password)
-      .subscribe((token) => console.log(token));
+    this.defaultService.loginCustomer(email, password).subscribe((token) => {
+      console.log(token);
+      this.localStorageService.set('sessionToken', token);
+
+      this.defaultService.defaultHeaders.set(
+        'Authorization',
+        `Bearer ${token}`
+      );
+      this.defaultService.findUserBySessionToken().subscribe((user) => {
+        console.log(user);
+        this.userSubject.next(user);
+      });
+    });
+    return this.user;
   }
 
-  logout() {
-    
-  }
-
+  logout() {}
 }
