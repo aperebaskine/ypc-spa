15fd43f71363742208ffdbc7fe130acdf3099d9f
Alexandre Perebaskine <pereb27@gmail.com>
2025-03-07 15:30:57 +0100

    Add form group

--- src/app/components/pages/login-page/login-page.component.ts
+++ src/app/components/pages/login-page/login-page.component.ts
@@ -1,18 +1,30 @@
 import { Location } from '@angular/common';
 import { Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { AuthenticationService } from '../../../services/authentication.service';
 
 @Component({
   selector: 'app-login-page',
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
 
-  constructor(private location: Location) {
+  loginForm = new FormGroup({
+    email: new FormControl(),
+    password: new FormControl()
+  });
+
+  constructor(private authService: AuthenticationService) {
+  }
+
+  login() {
+    let form = this.loginForm.value;
+    this.authService.login(form.email, form.password);
   }
 
 }
