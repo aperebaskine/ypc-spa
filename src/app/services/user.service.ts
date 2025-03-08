2f4d40be1afc44b27078c3e8671f17f6d696a39b
Alexandre Perebaskine <pereb27@gmail.com>
2025-03-08 17:41:54 +0100

    Implement service for retrieving user data

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,12 +1,34 @@
 import { Injectable } from '@angular/core';
-import { DefaultService } from '../generated';
+import { Customer, DefaultService } from '../generated';
+import { AuthenticationService } from './authentication.service';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private defaultService: DefaultService) {
+  private readonly userSubject!: BehaviorSubject<Customer | null>;
+  public readonly user!: Observable<Customer | null>;
+
+  constructor(
+    private defaultService: DefaultService,
+    private authService: AuthenticationService
+  ) {
+    this.userSubject = new BehaviorSubject<Customer | null>(null);
+    this.user = this.userSubject.asObservable();
+
+    this.authService.isAuthenticated.subscribe((isAuthenticated) => {
+      if (isAuthenticated) {
+        this.defaultService
+          .findUserBySessionToken()
+          .subscribe(
+            (user) => this.userSubject.next(user)
+          );
+      } else {
+        this.userSubject.next(null);
+      }
+    });
   }
-  
+
 }
