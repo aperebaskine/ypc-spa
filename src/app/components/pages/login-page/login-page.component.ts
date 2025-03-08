60208f3f68ccc3e25e0e9bdb8684bbe7423c4597
Alexandre Perebaskine <pereb27@gmail.com>
2025-03-08 17:49:06 +0100

    Fix formatting

--- src/app/components/pages/login-page/login-page.component.ts
+++ src/app/components/pages/login-page/login-page.component.ts
@@ -4,12 +4,18 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { AuthenticationService } from '../../../services/authentication.service';
-import { Observable, Observer } from 'rxjs';
+import { Observable } from 'rxjs';
 import { CommonModule, Location } from '@angular/common';
 
 @Component({
   selector: 'app-login-page',
-  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
@@ -33,8 +39,7 @@ export class LoginPageComponent {
     let form = this.loginForm.value;
     this.authService.login(form.email!, form.password!)
       .subscribe({
-        error: (error) => {
-          console.log("error");
+        error: () => {
           this.loginForm.reset();
           Object.values(this.loginForm.controls).forEach((control) =>
             control.setErrors({ 'incorrect': true }
