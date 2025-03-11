55fb086171d20aa7ce5ce0de5d5e60432ac70253
Alexandre Perebaskine <pereb27@gmail.com>
2025-03-11 14:31:21 +0100

    WIP 11/03/2025

--- src/app/components/user/dashboard-addresses/dashboard-addresses.component.ts
+++ src/app/components/user/dashboard-addresses/dashboard-addresses.component.ts
@@ -3,10 +3,11 @@ import { Address } from '../../../generated';
 import { CommonModule } from '@angular/common';
 import { AddressCardComponent } from "../../common/address/address-card/address-card.component";
 import { AddressService } from '../../../services/address.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-dashboard-addresses',
-  imports: [CommonModule, AddressCardComponent],
+  imports: [CommonModule, AddressCardComponent, MatButtonModule],
   templateUrl: './dashboard-addresses.component.html',
   styleUrl: './dashboard-addresses.component.scss'
 })
--- src/app/components/user/dashboard-orders/dashboard-orders.component.ts
+++ src/app/components/user/dashboard-orders/dashboard-orders.component.ts
@@ -3,10 +3,11 @@ import { OrderService } from '../../../services/order.service';
 import { CustomerOrder } from '../../../generated';
 import { OrderCardComponent } from "../../order/order-card/order-card.component";
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-dashboard-orders',
-  imports: [CommonModule, OrderCardComponent],
+  imports: [CommonModule, OrderCardComponent, MatButtonModule],
   templateUrl: './dashboard-orders.component.html',
   styleUrl: './dashboard-orders.component.scss'
 })
