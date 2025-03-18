b0b8dc63968c4ee523ca5795ce278ddec061c7a4
Alexandre Perebaskine <pereb27@gmail.com>
2025-03-18 13:57:55 +0100

    Format footer

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { MatListModule } from '@angular/material/list';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
-  imports: [MatToolbarModule, MatListModule],
+  imports: [MatIconModule, FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  faLinkedin = faLinkedin;
 }
