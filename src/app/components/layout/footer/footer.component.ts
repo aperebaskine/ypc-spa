ad48960f0c52c8a88a2f997a50e0da9d1a83cfba
Alexandre Perebaskine <pereb27@gmail.com>
2025-08-30 01:20:44 +0200

    Add Github link

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -11,4 +11,5 @@ import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 })
 export class FooterComponent {
   faLinkedin = faLinkedin;
+  faGithub = faGithub;
 }
