658b9aa48722986b748dbd1b8150b3d03fb7c9d6
Alexandre Perebaskine <pereb27@gmail.com>
2025-08-30 22:55:00 +0200

    Fix: Add loading state

--- src/app/components/dashboard/dashboard-orders/dashboard-orders.component.ts
+++ src/app/components/dashboard/dashboard-orders/dashboard-orders.component.ts
@@ -4,19 +4,25 @@ import { CustomerOrder } from '../../../generated';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { OrderPanelComponent } from "../../order/order-panel/order-panel.component";
+import { LoadingComponent } from "../../common/loading/loading.component";
 
 @Component({
   selector: 'app-dashboard-orders',
-  imports: [CommonModule, MatButtonModule, OrderPanelComponent],
+  imports: [CommonModule, MatButtonModule, OrderPanelComponent, LoadingComponent],
   templateUrl: './dashboard-orders.component.html',
   styleUrl: './dashboard-orders.component.scss'
 })
 export class DashboardOrdersComponent {
 
+  loading: boolean = true;
   orders?: CustomerOrder[];
 
   constructor(private orderService: OrderService) {
-    this.orderService.findAll().subscribe((orders) => this.orders = orders);
+    this.loading = true;
+    this.orderService.findAll().subscribe((orders) => {
+      this.orders = orders;
+      this.loading = false;
+    });
   }
 
 }
