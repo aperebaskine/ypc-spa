b4b47ae5bae7fe7ea7e1b64226483ee38b32f4ce
Alexandre Perebaskine <pereb27@gmail.com>
2025-10-01 16:38:58 +0200

    fix: Path for generated API client relative to project root folder

--- scripts/regenerate-api-client.bat
+++ scripts/regenerate-api-client.bat
@@ -1,2 +1,2 @@
 rmdir /S /Q src\\app\\generated
-openapi-generator-cli generate -i https://informaticapinguela.es/ypc-rest-api/api/openapi.json -g typescript-angular -o ../src/app/generated
+openapi-generator-cli generate -i https://informaticapinguela.es/ypc-rest-api/api/openapi.json -g typescript-angular -o ./src/app/generated
